f57700d097ea962067272829d4039865f5a684e0
grkumar0319 <71696705+grkumar0319@users.noreply.github.com>
2020-10-05 18:17:35 +0530

    adobe-assignment
    adobe-assignment

---
+++ precache-manifest.27b46103c31bf7ad0e159b2a05ddb1db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c7df79b333404e89d72c10fb5d13d8",
+    "url": "/adobe-assignment/index.html"
+  },
+  {
+    "revision": "b2e90a77555820a8b57a",
+    "url": "/adobe-assignment/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d43dc9784823f3f1f33b",
+    "url": "/adobe-assignment/static/css/main.3157024c.chunk.css"
+  },
+  {
+    "revision": "b2e90a77555820a8b57a",
+    "url": "/adobe-assignment/static/js/2.cbd7b7d4.chunk.js"
+  },
+  {
+    "revision": "50c5d34a26458e56f1667d34506f99ba",
+    "url": "/adobe-assignment/static/js/2.cbd7b7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43dc9784823f3f1f33b",
+    "url": "/adobe-assignment/static/js/main.6402aeea.chunk.js"
+  },
+  {
+    "revision": "048ae2aa759610b2dc71",
+    "url": "/adobe-assignment/static/js/runtime-main.2b814ed3.js"
+  },
+  {
+    "revision": "765fa5e338278a25135293d734dd5adc",
+    "url": "/adobe-assignment/static/media/i1.765fa5e3.jpg"
+  },
+  {
+    "revision": "7c415bb0448656ab83dd555d76d08fdb",
+    "url": "/adobe-assignment/static/media/i2.7c415bb0.jpg"
+  },
+  {
+    "revision": "31adf817db8fd13d29902afff30e482c",
+    "url": "/adobe-assignment/static/media/i3.31adf817.jpg"
+  }
+]);
